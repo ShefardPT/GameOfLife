50524ef8b6a7f44ddec0f7bbc9bff7ddd067099d
Shefer Alexander <shefard55r@gmail.com>
2019-11-12 18:20:07 +0300

    - main window was made, - TextBox_TextChangedEvent was handled

---
+++ GameOfLife.Core/Extensions/StringExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameOfLife.Core.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string SanitizeStringToNumeral(this string text)
+        {
+            var sb = new StringBuilder();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if ('0' < text[i] && text[i] < '9')
+                    sb.Append(text[i]);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
